f3443f41f449f07c3a0252b69aee35a7feee9b00
seoyoon lee <leeseoyoon01@gmail.com>
2025-09-24 15:45:32 +0900

    FEAT: Car 클래스 구현

---
+++ java-racingcar-6/src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,20 @@
+package racingcar.domain;
+
+
+public class Car {
+    private String name;
+    private Integer score;
+
+    public Car(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    public void move(){
+        score += 1;
+    }
+
+    public int getScore(){
+        return score;
+    }
+}
