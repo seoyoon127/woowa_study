7ddeaf7d8d36342e00d2abcac1acb04867b31ee7
seoyoon lee <leeseoyoon01@gmail.com>
2025-10-07 19:08:41 +0900

    feat: 오름차순 정렬 추가

--- src/main/java/Utils/RandomLotto.java
+++ src/main/java/Utils/RandomLotto.java
@@ -3,10 +3,13 @@ package Utils;
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.Domain.Lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class RandomLotto {
     public static Lotto getLottoNums(){
-        return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        List<Integer> lottoList = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(lottoList);
+        return new Lotto(lottoList);
     }
 }
