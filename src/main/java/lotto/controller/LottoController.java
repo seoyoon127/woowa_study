e8565029064c5bdff4271758c8132b4640c5a7bc
seoyoon lee <leeseoyoon01@gmail.com>
2025-10-10 21:40:29 +0900

    feat: Controller에 outputView 연결, Application 작성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -4,19 +4,27 @@ import lotto.domain.Lotto;
 import lotto.domain.LottoList;
 import lotto.domain.Result;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
-    InputView iv;
+    private InputView iv;
+    private OutputView ov;
+
+    public LottoController(InputView iv, OutputView ov) {
+        this.iv = iv;
+        this.ov = ov;
+    }
 
     public void run(){
         Integer cost = iv.input_cost();
         LottoList lottoList = LottoList.of(cost / 1000);
-        //로또번호 리스트 출력
+        ov.output_count(cost/1000);
+        ov.output_lottoList(lottoList.getLottoList());
 
         Lotto winNums = iv.input_winNums();
         Integer bonusNum = iv.input_bonusNum();
 
         Result result = Result.of(lottoList.getLottoList(), winNums.getNumbers(), bonusNum);
-        //당첨 통계 출력
+        ov.output_result(result, cost);
     }
 }
