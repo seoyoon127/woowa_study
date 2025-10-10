ab4ab6fb06664fbc50ee6ac8a127bc6e24dcdb8c
seoyoon lee <leeseoyoon01@gmail.com>
2025-10-10 12:55:16 +0900

    feat: Lotto 검증 메소드 구현

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validateLength(numbers);
+        validateDuplicate(numbers);
+        validateNumRange(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validateLength(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers){
+        if (new HashSet<>(numbers).size() != 6){
+            throw new IllegalArgumentException("[ERROR] 한 개의 로또에 중복된 번호가 있어선 안 됩니다.");
+        }
+    }
+
+    private void validateNumRange(List<Integer> numbers){
+        for (Integer num : numbers){
+            if (num < 1 || num > 45){
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+    }
+
+}
